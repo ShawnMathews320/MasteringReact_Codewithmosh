f3e8076069ef49028db4cab78e24312dad6ad889
shawn.mathews <shawn.mathews@stmartin.edu>
2022-02-07 16:48:44 -0800

    increment button with span

---
+++ react-app/src/components/counter.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+
+    // state includes any data that the component needs
+    state = {
+        count: 0
+    }
+
+    handleIncrement = product => {
+       this.setState({ count: this.state.count + 1 });
+    }
+
+    // renders our component into the UI
+    render() { 
+        return (
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button 
+                    onClick={(product) => this.handleIncrement(product)} 
+                    className='btn btn-secondary btn-sm'>
+                    Increment
+                </button>
+            </div>
+        );
+    }
+
+    // dynamically assigns bootstrap classes to our span next to the increment button
+    getBadgeClasses() {
+        let classes = 'badge m-2 badge-';
+        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        return classes;
+    }
+
+    // formats what the span next to the increment button should display according to its value
+    formatCount(){
+        // object destructuring for this.state
+        const { count } = this.state;
+
+        // ? means return what is right after it, : means otherwise return whats after that
+        return count === 0 ? 'Zero' : count;
+    }
+}
+ 
+export default Counter;
